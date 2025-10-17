98f6e1911b1ac3fe007eb97fd1e84be59233e545
Shreya Joshi <shreya@MacBook-Air-3.local>
2025-10-17 13:54:31 +1100

    My First Website

---
+++ script.js
@@ -0,0 +1,8 @@
+// This file adds interactivity to your website
+
+console.log("Welcome to my first website!");
+
+// Example: make the name clickable
+document.querySelector("h1").addEventListener("click", () => {
+  alert("You clicked my name!");
+});
